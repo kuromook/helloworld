af8539d4a4cbf923d341e67a850fcead26b31abc
kuromook <sh1@dream.jp>
2018-05-14 17:24:04 +0900

    selenium テストの chrome headless 化

--- test/test_selenium_test.py
+++ test/test_selenium_test.py
@@ -4,10 +4,14 @@
 import pytest
 from selenium import webdriver
 import chromedriver_binary 
+from selenium.webdriver.chrome.options import Options
 
 @pytest.mark.small
 def test():
-    browser = webdriver.Chrome()
+    options = Options()
+    options.add_argument('--headless')
+    browser = webdriver.Chrome(chrome_options=options)
+
     browser.get('http://0.0.0.0:8000')
     assert browser.find_element_by_tag_name('h1').text == 'Directory listing for /'
     lis = browser.find_elements_by_css_selector('li > a')
