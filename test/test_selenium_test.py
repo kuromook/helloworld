6c17bc6fa701734d45254e32fa8edd6b6e217c86
kuromook <sh1@dream.jp>
2018-05-14 18:53:12 +0900

    selenium test

--- test/test_selenium_test.py
+++ test/test_selenium_test.py
@@ -14,5 +14,5 @@ def test():
 
     browser.get('http://0.0.0.0:8000')
     assert browser.find_element_by_tag_name('h1').text == 'Directory listing for /'
-    lis = browser.find_elements_by_css_selector('li > a')
-    assert lis[8].get_attribute('href') == 'http://0.0.0.0:8000/simpleserver.py'
+
+
